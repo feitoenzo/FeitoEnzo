d431188afc3bd3fd4db215a313bdeaaedcb35570
Enzo <feitoenzo@gmail.com>
2023-01-31 21:50:46 -0300

    trabajo terminado

--- js/main.js
+++ js/main.js
@@ -1,82 +1,44 @@
-/* 
 
 
 var addingUsers = true;
 var users = [];
-
-function addUser() {
-    if (addingUsers) {
-        var nombre1 = document.getElementById("nombre1").value;
-        var monto1 = document.getElementById("monto1").value;
-        users.push({ nombre1: nombre1, monto1: monto1 });
-        displayUsers();
-    }
-}
-
-function stopAddingUsers() {
-    addingUsers = false;
-    document.getElementById("calculate").style.display = "block";
-    document.getElementById("nombre1").style.display = "none";
-    document.getElementById("monto1").style.display = "none";
-    document.getElementById("addUser").style.display = "none";
-    document.getElementById("stopAddingUsers").style.display = "none";
-}
-
-
-
-function calculatePromedio() {
-    var total = 0;
-    for (var i = 0; i < users.length; i++) {
-        total += parseFloat(users[i].monto1);
-    }
-    var promedio = total / users.length;
-    document.getElementById("result").innerHTML = "El total es de $" + total + "<br> El promedio es de $" + promedio + "<br>";
-    return total;
-}
-
-
-
-
-
-function displayUsers() {
-    var list = document.getElementById("userList");
-    var item = document.createElement("li");
-    item.innerHTML = document.getElementById("nombre1").value + " - $" + document.getElementById("monto1").value;
-    list.appendChild(item);
-}
-
-  */
-
-
-
-//=============================================================================================================
-
-
-
-var addingUsers = true;
-var users = [];
-
+//agregar usuarios//  sweet alert en las alertas
 function addUser() {
     var nombre1 = document.getElementById("nombre1").value;
     var monto1 = document.getElementById("monto1").value;
     
 
     if (nombre1 === '' || monto1 === '') {
-        alert('Por favor, ingrese un nombre y un monto válidos');
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor, ingrese un nombre y un monto válidos',
+            icon: 'error',
+            showConfirmButton: false,
+            background: '#1c1c1e'
+        });
         return;
     }
     
     if (isNaN(monto1)) {
-        alert('Por favor, ingrese un monto válido');
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor, ingrese un monto válido',
+            icon: 'error',
+            showConfirmButton: false,
+            background: '#1c1c1e'
+        });
         return;
     }
 
-    users.push({ nombre1: nombre1, monto1: monto1 });
+   users.push({ nombre1: nombre1, monto1: monto1 });
     displayUsers();
     document.getElementById("nombre1").value = '';
     document.getElementById("monto1").value = '';
 }
 
+
+
+//boton para detener la carga usarios, una vez apretado el boton "listo" los campos desaparecen.
 function stopAddingUsers() {
     addingUsers = false;
     document.getElementById("calculate").style.display = "block";
@@ -90,7 +52,7 @@ function stopAddingUsers() {
 
 
 
-
+//esta funcion calcula el monto total y el prmedio, tambien genera lista de deudores (quienes pusieron menos del prmedio) y acreedores (quienes pusieron mas)
 function calculatePromedio() {
     var total = 0;
    
@@ -105,8 +67,8 @@ function calculatePromedio() {
     var deudoresList = document.getElementById("deudoresList");
     var acreedoresList = document.getElementById("acreedoresList");
     var libresList = document.getElementById ("libresList");
-    deudoresList.innerHTML = "Deudores:<br>";
-    acreedoresList.innerHTML = "Acreedores:<br>";
+    deudoresList.innerHTML = "Deudores: Los siguientes usuarios deben poner la cantidad indicada de dinero para balancear las cuentas.<br>";
+    acreedoresList.innerHTML = "Acreedores: Los siguientes usuarios deben recibir la cantidad indicada de dinero para balancear las cuentas.<br>";
     for (var i = 0; i < users.length; i++) {
         if (users[i].monto1== promedio){
             var item = document.createElement("li");
@@ -126,7 +88,7 @@ function calculatePromedio() {
     return total;
 }
 
-
+// esta funcion imprime a los usuarios a medida que se agregan.
 function displayUsers() {
     var list = document.getElementById("userList");
     var item = document.createElement("li");
@@ -134,7 +96,7 @@ function displayUsers() {
     list.appendChild(item);
 }
 
-
+// funcion para resetear la carga de datos de usuarios
 function resetButton() {
     document.getElementById("nombre1").value = '';
     document.getElementById("monto1").value = '';
@@ -148,9 +110,9 @@ function resetButton() {
     users = [];
     addingUsers = true;
 }
+  
+
  
-
-
 
 
 
